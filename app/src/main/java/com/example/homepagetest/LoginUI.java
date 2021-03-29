9e2c6ed3694be8c198846fb6f8544eb55e203ed1
wanganyi1102 <62319036+wanganyi1102@users.noreply.github.com>
2021-03-29 15:19:39 +0800

    committed 28/3

--- app/src/main/java/com/example/homepagetest/LoginUI.java
+++ app/src/main/java/com/example/homepagetest/LoginUI.java
@@ -17,100 +17,100 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-//public class LoginUI extends AppCompatActivity {
-//
-//    EditText mEmailPhone, mPassword;
-//    Button LoginBtn;
-//    TextView mRegBtn;
-//    FirebaseAuth mAuth;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.login_ui);
-//
-//        mEmailPhone = findViewById(R.id.EmailPhoneNo);
-//        mPassword = findViewById(R.id.Password); //Idk if this is same
-//        LoginBtn = findViewById(R.id.LoginBtn);
-//        mRegBtn = findViewById(R.id.CreateAccTxt);
-//        mAuth = FirebaseAuth.getInstance();
-//
-//        LoginBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //have to change to email/phoneNumber (need some logic)
-//                String email = mEmailPhone.getText().toString().trim();
-//                String password = mPassword.getText().toString().trim();
-//
-//                if (TextUtils.isEmpty(email)) {
-//                    mEmailPhone.setError("Email is Required.");
-//                    return;
-//                }
-//
-//                if (TextUtils.isEmpty(password)) {
-//                    mPassword.setError("Password is Required.");
-//                    return;
-//                }
-//
-//                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            Toast.makeText(LoginUI.this, "Login successful", Toast.LENGTH_SHORT).show();
-//                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-//                        } else {
-//                            Toast.makeText(LoginUI.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
-//            }
-//        });
-//
-//        mRegBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(getApplicationContext(), RegisterUI.class));
-//            }
-//        });
-//
-//    }
-//}
 public class LoginUI extends AppCompatActivity {
+
+    EditText mEmailPhone, mPassword;
+    Button LoginBtn;
+    TextView mRegBtn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login_ui);
 
-        Button loginBtn = (Button) findViewById(R.id.loginBtn); //declare login btn
-        Login login = new Login();
-        loginBtn.setOnClickListener(new View.OnClickListener() { //upon clicking on login btn
+        mEmailPhone = findViewById(R.id.EmailPhoneNo);
+        mPassword = findViewById(R.id.Password); //Idk if this is same
+        LoginBtn = findViewById(R.id.LoginBtn);
+        mRegBtn = findViewById(R.id.CreateAccTxt);
+        mAuth = FirebaseAuth.getInstance();
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                EditText PhoneEmailEditText = (EditText) findViewById(R.id.PhoneEmailEditText);
-                EditText enterPasswordEditText = (EditText) findViewById(R.id.enterPasswordEditText);
-                String phoneEmail = PhoneEmailEditText.getText().toString(); //get texts entered
-                String password = enterPasswordEditText.getText().toString();
+            public void onClick(View view) {
+                //have to change to email/phoneNumber (need some logic)
+                String email = mEmailPhone.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
-                //if(login.checkExist(phoneEmail) == true){ //login successful
-                    Intent loginSuccessIntent = new Intent(getApplicationContext(), HomePageUI.class);
-                    loginSuccessIntent.putExtra("com.example.ACCOUNT", "xiaoming123");
-                    startActivity(loginSuccessIntent);
-                /*}
-                else{
-                    System.out.println("not successful");
-                }*/
+                if (TextUtils.isEmpty(email)) {
+                    mEmailPhone.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoginUI.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomePageUI.class));
+                        } else {
+                            Toast.makeText(LoginUI.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
 
-        TextView registerBtn = (TextView) findViewById(R.id.registerBtn);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(getApplicationContext(), RegisterUI.class);
-                startActivity(registerIntent);
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterUI.class));
             }
         });
 
     }
 }
+//public class LoginUI extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//
+//        Button loginBtn = (Button) findViewById(R.id.loginBtn); //declare login btn
+//        Login login = new Login();
+//        loginBtn.setOnClickListener(new View.OnClickListener() { //upon clicking on login btn
+//            @Override
+//            public void onClick(View v) {
+//                EditText PhoneEmailEditText = (EditText) findViewById(R.id.PhoneEmailEditText);
+//                EditText enterPasswordEditText = (EditText) findViewById(R.id.enterPasswordEditText);
+//                String phoneEmail = PhoneEmailEditText.getText().toString(); //get texts entered
+//                String password = enterPasswordEditText.getText().toString();
+//
+//                //if(login.checkExist(phoneEmail) == true){ //login successful
+//                    Intent loginSuccessIntent = new Intent(getApplicationContext(), HomePageUI.class);
+//                    loginSuccessIntent.putExtra("com.example.ACCOUNT", "xiaoming123");
+//                    startActivity(loginSuccessIntent);
+//                /*}
+//                else{
+//                    System.out.println("not successful");
+//                }*/
+//            }
+//        });
+//
+//        TextView registerBtn = (TextView) findViewById(R.id.registerBtn);
+//        registerBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent registerIntent = new Intent(getApplicationContext(), RegisterUI.class);
+//                startActivity(registerIntent);
+//            }
+//        });
+//
+//    }
+//}
