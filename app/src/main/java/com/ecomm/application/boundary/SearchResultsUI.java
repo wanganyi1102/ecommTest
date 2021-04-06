27facc5c30b41ebe4e16f98b50e944d33001e951
joelle <joellecenedra@gmail.com>
2021-04-06 15:38:53 +0800

    kjo

---
+++ app/src/main/java/com/ecomm/application/boundary/SearchResultsUI.java
@@ -0,0 +1,16 @@
+package com.ecomm.application.boundary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ecomm.application.R;
+
+public class SearchResultsUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results_u_i);
+    }
+}
