04c18adae37902c361077587707771588adf137b
joelle <joellecenedra@gmail.com>
2021-04-06 14:42:26 +0800

    kjo

--- app/src/main/java/com/ecomm/application/SearchResults.java
+++ app/src/main/java/com/ecomm/application/SearchResults.java
@@ -1,7 +1,5 @@
-package com.ecomm.application.boundary;
+package com.ecomm.application;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -12,19 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-import com.ecomm.application.R;
+public class SearchResults extends AppCompatActivity {
 
-public class SearchResultsUI extends AppCompatActivity {
-
-    Button backHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results_u_i);
+        setContentView(R.layout.activity_search_results);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
@@ -38,17 +30,5 @@ public class SearchResultsUI extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        //Set back button to activity
-        backHome = findViewById(R.id.backHome);
-        backHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent backHomeIntent = new Intent(getApplicationContext(), HomePageUI.class);
-                startActivity(backHomeIntent);
-            }
-        });
     }
-
-
 }
