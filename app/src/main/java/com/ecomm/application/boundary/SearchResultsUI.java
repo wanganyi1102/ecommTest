fab5962624d0a75af12c321b4a2cbb7772bc5b6e
joelle <joellecenedra@gmail.com>
2021-04-06 15:45:15 +0800

    kjo

--- app/src/main/java/com/ecomm/application/boundary/SearchResultsUI.java
+++ app/src/main/java/com/ecomm/application/boundary/SearchResultsUI.java
@@ -1,16 +1,41 @@
 package com.ecomm.application.boundary;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.ecomm.application.R;
 
 public class SearchResultsUI extends AppCompatActivity {
 
+    Button backHome;
+    @SuppressLint("MissingSuperCall")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        //Set back button to activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_results_u_i);
+        backHome = findViewById(R.id.backHome);
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backHomeIntent = new Intent(getApplicationContext(), HomePageUI.class);
+                startActivity(backHomeIntent);
+            }
+        });
     }
+
+
 }
