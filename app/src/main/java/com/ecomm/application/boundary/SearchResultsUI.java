3359e1be01867416d714f281226a4147c4d3e468
joelle <joellecenedra@gmail.com>
2021-04-08 23:11:55 +0800

    dfsdc

--- app/src/main/java/com/ecomm/application/boundary/SearchResultsUI.java
+++ app/src/main/java/com/ecomm/application/boundary/SearchResultsUI.java
@@ -32,12 +32,14 @@ public class SearchResultsUI extends AppCompatActivity {
 
     Button backHome;
     webCrawl crawler = new webCrawl();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
-        //Set back button to activity
+        //back to home button
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results_u_i);
+
         backHome = findViewById(R.id.backHome);
         backHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,8 +49,50 @@ public class SearchResultsUI extends AppCompatActivity {
             }
         });
 
-        //textViews
+
+
+
+
+        //initialise all textViews
         TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+
+        SearchView SearchBar = (SearchView) findViewById(R.id.searchBar);
+        SearchBar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // Functionality for the button...
+                Intent searchPage = new Intent(getApplicationContext(), SearchResultsUI.class);
+                startActivity(searchPage);
+            }
+        });
+
+        /*
+        final SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Intent intent = new Intent(getApplicationContext(), SearchResultsUI.class);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return true;
+
+            }
+        };
+
+
+        SearchBar.setOnQueryTextListener(queryTextListener);
+
+         */
+
+
+
         ArrayList<Product> productList = new ArrayList<Product>();
 
         try {
@@ -78,8 +122,8 @@ public class SearchResultsUI extends AppCompatActivity {
 
         System.out.println(productList.get(1).getPrice());
         System.out.println(productList.get(1).getName());
-        System.out.println(productList.get(1).getURL());
-        System.out.println(productList.get(1).getecommerceSite());
+        System.out.println(productList.get(1).getUrl());
+        System.out.println(productList.get(1).getEcommerceSite());
 
 
         //System.out.println(Arrays.toString(productList.toArray()));
