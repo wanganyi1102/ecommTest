338d7beef2f799971b8a0787f4ea66fef51e6d95
kazuki <lwenyu0110@gmail.com>
2021-04-12 12:49:05 +0800

    add quantity

--- app/src/main/java/com/ecomm/application/boundary/ShoppingCartUI.java
+++ app/src/main/java/com/ecomm/application/boundary/ShoppingCartUI.java
@@ -30,6 +30,7 @@ public class ShoppingCartUI extends AppCompatActivity {
     ArrayList<String> imageURLs = new ArrayList<>();
 //    ArrayList<String> ratings = new ArrayList<>();
     ArrayList<String> prices = new ArrayList<>();
+    ArrayList<Integer> quantity = new ArrayList<>();
     public static TextView orderTotalTextView;
     public static double total = 0.0;
 
@@ -143,13 +144,15 @@ public class ShoppingCartUI extends AppCompatActivity {
             imageURLs.add(p.getImageURL());
 //            ratings.add(p.getRating()+"");
             prices.add(p.getPrice()+"");
+            quantity.add(p.getQuantity());
+
         }
         initRecyclerView();
     }
 
     private void initRecyclerView(){
         RecyclerView cartRecyclerView = findViewById(R.id.cartRecyclerView);
-        CartListAdapter adapter = new CartListAdapter(titles, imageURLs, prices, this);
+        CartListAdapter adapter = new CartListAdapter(titles, imageURLs, prices, quantity, this);
         cartRecyclerView.setAdapter(adapter);
         cartRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     };
